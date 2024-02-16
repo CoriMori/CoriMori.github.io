aead4ab82b9c40becc173209c8373dc04472ef11
Cori Mori <cori1753@gmail.com>
2024-02-16 13:36:55 -0500

    New images and updated work

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.use(express.static(path.join(`${__dirname} /../client/public`)));
+app.use(express.static(path.join(`${__dirname} /..`)));
 app.get('/', (req, res) => {
     res.sendFile(path.join(`${__dirname} /../client/index.html`));
 });
